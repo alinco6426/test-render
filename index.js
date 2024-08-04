0d53985718545aeda319bfcafc78afa1ca4e1912
adegbenro marvellous <alinco2319@gmail.com>
2024-08-05 00:42:47 +0100

    new route added

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const cors = require("cors");
 
 const port =  8000;
 
-app.use(cors())
+app.use(cors());
+
+app.use(express.json())
 
 app.get("/" ,  (req , res) => {
      res.send("Hello from server")
+});
+
+app.get("/movies" , (req, res) => {
+     const movies = [
+          { id: 1, name: "Movie 1" },
+          { id: 2, name: "Movie 2" },
+     ];
+     res.json(movies);
 })
 app.listen(port ,() => {
      console.log("server running" , port);
